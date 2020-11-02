d0d8db2e668838a5743d531ee26a14b6c7b13722
Sergey Semenov <dindrophone@gmail.com>
2020-11-02 09:50:40 +0300

    [vue] add popover

---
+++ src/components/+atoms/PopoverConfirm.vue
@@ -0,0 +1,23 @@
+<template>
+	<span class="popover-confirm">
+		<slot></slot>
+	</span>
+</template>
+
+<script>
+export default {
+	props: {
+		title: String,
+	},
+
+	mounted() {
+		const element = this.$slots.default[0].elm;
+
+		element.addEventListener('click', (e) => {
+			e.preventDefault();
+			const confirm = window.confirm(this.title);
+			confirm && this.$emit('confirm');
+		});
+	},
+}
+</script>
